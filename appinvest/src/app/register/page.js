a09a802f7e908067288529a1bd57eb7e473c9da5
Andronique <andronique64@gmail.com>
2025-06-29 15:00:43 +0300

    reparation build

--- appinvest/src/app/register/page.js
+++ appinvest/src/app/register/page.js
@@ -1,6 +1,11 @@
 // src/app/register/page.jsx
 import Registre from '../../components/Registre';
-
+import { Suspense } from 'react';
 export default function RegisterPage() {
-  return <Registre language="fr" />;
+    return (
+    <Suspense fallback={<div>Chargement...</div>}>
+      <Registre language="fr" />
+    </Suspense>
+  );
 }
+
